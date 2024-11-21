15aef4f23604ad2f3a14971db04066e71dc0e6d0
Marki <124947044+MarkiHub@users.noreply.github.com>
2024-11-20 19:14:57 -0700

    feat: pagar con tarjeta

--- DogosFrontend/CobrarOrden/CobrarOrden.js
+++ DogosFrontend/CobrarOrden/CobrarOrden.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const montoInput = document.getElementById('monto');
     const cambioSpan = document.getElementById('cambio');
     const finalizarOrdenBtn = document.querySelector('.finalizarOrden');
+    const finalizarOrdenTarjetaBtn = document.querySelector('.finalizarOrdenTarjeta');
     const API_URL = 'http://localhost:3000';
-
+    
     montoInput.addEventListener('input', () => {
         const montoPagado = parseFloat(montoInput.value) || 0;
         const totalElement = document.querySelector('total-summary').shadowRoot.querySelector('#totalOrden');
@@ -20,50 +21,56 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    async function pagar(){
-
-    };
-
-    finalizarOrdenBtn.addEventListener('click', async () =>{
+    const finalizarOrden = async () => {
         const montoPagado = parseFloat(montoInput.value) || 0;
         const totalElement = document.querySelector('total-summary').shadowRoot.querySelector('#totalOrden');
         const total = parseFloat(totalElement.textContent.replace('$', '')) || 0;
-    
-        if (montoPagado >= total) {
-            try {
-                const ordenJSON = sessionStorage.getItem('ordenJSON');
-                const orden = JSON.parse(ordenJSON);
-                const fecha = new Date().toISOString();
-                const idUsuario = 1; 
 
-                const response = await fetch(`${API_URL}/orden`, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        numero: orden.numero,
-                        fechaHora: fecha,
-                        total: total,
-                        usuarioId: idUsuario,
-                        detalleOrden: orden.detalleOrden,
-                    }),
-                });
-    
-                if (!response.ok) {
-                    throw new Error('Error al guardar la orden');
-                }else{
-                    sessionStorage.removeItem('ordenJSON');
-                    alert(`Orden realizada con éxito.`);
-                    history.go(-1);
-                }
-        
+        try {
+            const ordenJSON = sessionStorage.getItem('ordenJSON');
+            const orden = JSON.parse(ordenJSON);
+            const fecha = new Date().toISOString();
+            const idUsuario = 1;
 
-            } catch (error) {
-                alert('Ocurrió un error al guardar la orden. Inténtalo de nuevo.');
+            const response = await fetch(`${API_URL}/orden`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    numero: orden.numero,
+                    fechaHora: fecha,
+                    total: total,
+                    usuarioId: idUsuario,
+                    detalleOrden: orden.detalleOrden,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al guardar la orden');
+            } else {
+                sessionStorage.removeItem('ordenJSON');
+                alert(`Orden realizada con éxito.`);
+                history.go(-1);
             }
+
+        } catch (error) {
+            alert('Ocurrió un error al guardar la orden. Inténtalo de nuevo.');
+        }
+    };
+
+    finalizarOrdenBtn.addEventListener('click', async () => {
+        const montoPagado = parseFloat(montoInput.value) || 0;
+        const totalElement = document.querySelector('total-summary').shadowRoot.querySelector('#totalOrden');
+        const total = parseFloat(totalElement.textContent.replace('$', '')) || 0;
+
+        if (montoPagado >= total) {
+            finalizarOrden();
         } else {
             alert('El monto pagado no es suficiente para cubrir el total.');
         }
+    });
 
+    finalizarOrdenTarjetaBtn.addEventListener('click', () => {
+        finalizarOrden();
     });
     
 
